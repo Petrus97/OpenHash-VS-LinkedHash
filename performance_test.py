9126b80d6a0fd35d5e11b8eb48602bd79efbc07a
Petrus97 <ale199765@live.it>
2020-08-27 11:32:50 +0200

    add figures

--- performance_test.py
+++ performance_test.py
@@ -2,6 +2,7 @@ import OpenHash
 import LinkedHash
 import CollisionCounter
 import pandas as pd
+import matplotlib.pyplot as plt
 import random
 import time
 import os
@@ -19,14 +20,18 @@ def insert_test(m):
 	hash.attach(collision_counter)
 	l_hash.attach(l_collision_counter)
 	df = pd.DataFrame(columns = ['α','OpenHash', 'HashCollision', 'LinkedHash', 'L_HashCollision'])
-	charge_factor = [0.25, 0.50, 0.75, 0.90, 0.99]
+	charge_factor = []
+	alpha = 0.1
+	while alpha <= 1:
+		charge_factor.append('%.2f'%alpha)
+		alpha += 0.05
 	df["α"] = charge_factor
 	collisions = []
 	l_collisions = []
 	elapsed = []
 	l_elapsed = []
 	values = [] # extract from file the values and put in this array
-	file_name = "db/file_test_" + str(m) + ".txt"
+	file_name = "db/file_test_" + str(m) + ".pickle"
 	with open(file_name, "rb") as file_test:
 		count = 0
 		while count < m:
@@ -39,11 +44,11 @@ def insert_test(m):
 	for alpha in charge_factor:
 		n = 0
 		start = time.perf_counter()
-		while n/hash.table_size <= alpha:
+		while n/hash.table_size <= float(alpha):
 			hash.hash_insert(values[n])
 			n += 1
 		end = time.perf_counter()
-		elapsed.append(end - start)
+		elapsed.append('%.4f'%(end-start))
 		collisions.append(collision_counter.get_collision())
 		collision_counter.reset_counter()
 		hash.empty_table()
@@ -51,11 +56,11 @@ def insert_test(m):
 	for alpha in charge_factor:
 		n = 0
 		start = time.perf_counter()
-		while n/l_hash.table_size <= alpha:
+		while n/l_hash.table_size <= float(alpha):
 			l_hash.linked_insert(values[n])
 			n += 1
 		end = time.perf_counter()
-		l_elapsed.append(end - start)
+		l_elapsed.append('%.4f'%(end-start))
 		l_collisions.append(l_collision_counter.get_collision())
 		l_collision_counter.reset_counter()
 		l_hash.empty_table()
@@ -64,12 +69,25 @@ def insert_test(m):
 	df["HashCollision"] = collisions
 	df["L_HashCollision"] = l_collisions
 	print(df)
+	plt.xlabel("Charging factor α")
+	plt.ylabel("Number of collisions")
+	plt.figure(figsize=(12.80, 7.2))
+	plt.plot(df["α"], collisions, marker="o")
+	plt.plot(df["α"], l_collisions, marker="o")
+	plt.legend(['open hash', 'linked hash'], loc='upper left')
+	plt.title("Collision number")
+	plt.suptitle("OpenHash vs LinkedHash")
+	namefile = "collision_test_on_" + str(m) + "_table"
+	plt.savefig('figures/' + namefile + ".png")
+	plt.clf()
+	df.to_latex("results/" + namefile + ".tex")
+
 
 '''
 This function generate values for evaluate the insert test
 '''
 def generate_values(table_dim):
-	file_name = "db/file_test_" + str(table_dim) + ".txt"
+	file_name = "db/file_test_" + str(table_dim) + ".pickle"
 	file = open(file_name, 'wb+')
 	for i in range (0, table_dim):
 		pickle.dump(random.randrange(0, 10000), file)
